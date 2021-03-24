a8621a001606beb328998f35521e7bd1dbbef6c8
Hamzah Kirana <80812572+Boy-Tolkit@users.noreply.github.com>
2021-03-24 15:10:55 +0700

    Create Spesial.c

---
+++ Spesial.c
@@ -0,0 +1,10 @@
+import requests,bs4,sys,os,subprocess
+import requests,sys,random,time,re,base64,json
+
+try:
+   pass
+
+print("Script Nya Di Perbarui Dulu Broo")
+
+if __name__=='__main__':
+  menu()
